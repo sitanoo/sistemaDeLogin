c47ac03fa70c84721b9697644ef8d91d6e1df67f
sitanoo <47368164+sitanoo@users.noreply.github.com>
2019-06-27 21:40:22 -0300

    closes #3 closes #8
    rede problema

--- index.php
+++ index.php
@@ -10,6 +10,12 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <title>Sistema de Login</title>
+    <style>
+        #alerta,#caixaRegistro,#caixaSenha{
+            display: none;
+        }
+        
+    </style>
   </head>
   <body class="bg-dark">
       <main class="container mt-4">
@@ -78,35 +84,88 @@
           </section>
           
           <br>
-          
-          <!-- Formulário de Recuperação de Senha -->
-          <section classe="row">
-              <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaSenha">
-                  <h2 class="text-center mt-2">
-                      Gerar nova senha
-                  </h2>
-                  <form action="#" method="post" role="form" class="p-2" id="formSenha">
+          <section class="row">
+              <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaRegistro">
+                  <h2 class="text-center mt-2">Registrar-se</h2>
+                  <form action="#" method="post" role="form" class="p-2" id="formRegistro">
+                      
                       <div class="form-group">
-                          <small class="text-muted">
-                              Para gerar nova senha, digite seu e-mail
-                          </small>
-                      </div>
-                      <div class="form-group">
-                          <input type="email" name="emailGerarSenha" class="form-control" placeholder="E-mail" required="">
+                          <input type="text" name="nomeCompleto" class="form-control" placeholder="Nome Completo" required minlength="6">
                       </div>
                       
                       <div class="form-group">
-                          <input type="submit" name="btnGerar" id="btnGerar" value="Gerar" class="btn btn-primary btn-block">
+                          <input type="text" name="NomeUsuario" class="form-control" placeholder="Nome do Usuário" required minlength="6">   
                       </div>
-                      <div class="form-group float-right">
-                          <a href="#" id="btnVoltar">
-                              voltar
-                          </a>
+                      
+                      <div class="form-group">
+                          <input type="email" name="EmailUsuario" class="form-control" placeholder="E-mail" required>   
                       </div>
+                      
+                      <div class="form-group">
+                          <input type="password" id="SenhaUsuário" class="form-control" placeholder="Senha" required minlength="6">   
+                      </div>
+                      
+                      <div class="form-group">
+                          <input type="password" id="SenhaUsuárioConfirmar" class="form-control" placeholder="Confirmar a Senha" required minlength="6">   
+                      </div>
+                      
+                      <div class="form-group">
+                          <div class="custom-control custom-checkbox">
+                          <input type="checkbox" name="concordar" class="custom-control-input" id="checkConcordar">
+                          <label for="checkConcordar" class="custom-control-label">
+                              Eu concordo com os <a href="#">termos e condições.</a>
+                              
+                          </label>
+                          </div>
+                      </div>
+                      
+                      <div class="form-group">
+                          <input type="submit" name="btnRegistroUsuario" id="btnRegistroUsuario" value="Registrar" class="btn btn-primary btn-block">
+                      </div>
+                      
+                      <div class="form-group">
+                          <p class="text-center">
+                              Já registrado?
+                              <a href="#" id="btnEntrarRegistrado">
+                                  Entrar aqui.
+                              </a>
+                          </p>
+                      </div>
+                      
                       
                   </form>
               </div>
           </section>
+          <br>
+          
+          <!-- Formulário de Recuperação de Senha -->
+          
+           <section class="row">
+             <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaSenha">
+                 <h2 class="text-center mt-2">
+                     Gerar Nova Senha.
+                 </h2>
+                 <form action="#" method="post" role="Form" class="p-2" id="formSenha">
+                     <div class="form-group">
+                         <small class="text-muted">
+                             Para Gerar Nova Senha Digite Seu E-mail, Assim Recebendo as Instruçoes.
+                         </small>
+                     </div>
+                     <div class="form-group">
+                         <input type="email" name="emailGerarSenha" class="form-control" placeholder="E-mail" required="">
+                     </div>
+                     <div class="form-group">
+                         <input type="submit" name="btnGerar" id="btnGerar" value="Gerar" class="btn btn-primary btn-block">
+                     </div>
+                     <div class="form-group float-right">
+                         <a href="#" id="btnVoltar">
+                             Voltar
+                         </a>
+                     </div>
+                 </form>
+             </div>
+        </section>
+              
          
           
       </main>
@@ -118,5 +177,37 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script>
+        //$(document).ready(function(){});
+        //jQuery
+        $(function(){
+            $("#btnEsqueci").click(function(){
+                $("#caixaLogin").hide();
+                $("#caixaSenha").show();
+            });
+        });
+        
+        $(function(){
+            $("#btnRegistrar").click(function(){
+                $("#caixaLogin").hide();
+                $("#caixaRegistro").show();
+            });
+        });
+        
+        $(function(){
+            $("#btnEntrarRegistrado").click(function(){
+                $("#caixaRegistro").hide();
+                $("#caixaLogin").show();
+            });
+        });
+        
+        $(function(){
+            $("#btnVoltar").click(function(){
+                $("#caixaSenha").hide();
+                $("#caixaLogin").show();
+            });
+        });
+        
+    </script>
   </body>
 </html>
